262b746a2f1a2fda77ae9b200849e596484be9e3
Kia Boluki <Kia.Boluki@gmail.com>
2021-11-06 12:49:30 +0330

    LazyLoad Added

--- src/components/Poster.js
+++ src/components/Poster.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 const base_url = "https://image.tmdb.org/t/p/original";
 
 function Poster({movie, isLargeRow, onClick}) {
     return (
         <div className="movie__container" key={movie.id} onClick={onClick}>
-        <img className="movie__poster" src={`${base_url}${isLargeRow ? movie?.poster_path : movie?.backdrop_path || movie?.poster_path}` } alt={movie.name || movie.title} />
+          <LazyLoadImage className="movie__poster" src={`${base_url}${isLargeRow ? movie?.poster_path : movie?.backdrop_path || movie?.poster_path}` } alt={movie.name || movie.title} />
         <p>{movie.name || movie.title }</p>
       </div>
     )
